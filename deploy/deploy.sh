4a0d38462f75230e6a2bad3865fc2b230902c268
Romain Tourte <rtourte@yahoo.fr>
2025-04-14 11:23:54 +0200

    Update install

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,23 @@
-# Node-RED Install / Config
+# -----------
+### NODE JS
+# -----------
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo bash - 
+sudo apt-get install -y nodejs
+
+# -----------
+### INFLUXDB
+# -----------
+sudo apt update
+sudo apt install -y gnupg2 curl wget
+wget -qO- https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+echo "deb https://repos.influxdata.com/debian bulleye stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+
+sudo apt update
+sudo apt install -y influxdb
+
+# -----------
+### NODE-RED
+# -----------
 # Process > https://nodered.org/docs/getting-started/raspberrypi
 cd
 echo "######### Installation Node-Red"
@@ -10,9 +29,15 @@ npm install node-red-dashboard
 # InfluxDB component - https://flows.nodered.org/node/node-red-contrib-influxdb
 npm install node-red-contrib-influxdb
 
+# -----------
+### MOSQUITTO
+# -----------
 echo "######### Installation Mosquitto"
 sudo apt install -y mosquitto
 
+# -----------
+### PYTHON
+# -----------
 echo "######### Installation python & dépendances"
 sudo apt-get -y install python3-pip
 sudo pip3 install paho-mqtt --break-system-packages
