6c039fd78b3cbaf6a7a991c32befe6cc7da9e947
Romain Tourte <rtourte@yahoo.fr>
2025-04-14 12:03:05 +0200

    Update deloy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,8 +1,12 @@
 # -----------
 ### NODE JS
 # -----------
-curl -fsSL https://deb.nodesource.com/setup_20.x | sudo bash - 
-sudo apt-get install -y nodejs
+sudo apt install -y ca-certificates curl gnupg
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /usr/share/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt update
+sudo apt install -y nodejs
 
 # -----------
 ### INFLUXDB
@@ -47,3 +51,6 @@ sudo cp ~/code/PiloteOTT/deploy/piloteOTT.service.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable piloteOTT.service.service
 sudo systemctl start piloteOTT.service.service
+
+# INSTALL DU RELAY 
+# Procédure : https://www.waveshare.com/wiki/RPi_Relay_Board_(B)#Others
